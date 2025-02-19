1a1e1a8d89f6d282ae905f9f3151fde205123a7b
33ban08 <mr.mohmaedboulahka@gmail.com>
2025-02-19 16:18:52 +0100

    Message décrivant les modifications

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import Image from 'next/image';
 import { HiOutlinePencil } from "react-icons/hi2";
-import { BiLogIn } from "react-icons/bi";
+import { BiLogIn, BiLogOut } from "react-icons/bi";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Header() {
   const USER_IMAGE = 'https://res.cloudinary.com/dknvsbuyy/image/upload/v1686314044/1617826370281_30f9a2a96a.jpg';
   const { data: session } = useSession();
-  console.log(session)
+  console.log(session);
   
   return (
     <div className='flex justify-between items-center w-full border-blue-500 border-b-[2px] p-5'>
@@ -19,17 +19,19 @@ export default function Header() {
           <span className='hidden sm:block'>CREATE POST</span> 
           <HiOutlinePencil className='sm:hidden' />
         </button>
+        
         {session ? (
-          <>
-            <button className='px-3 py-2 bg-gray-700 text-white rounded-full' onClick={() => signOut()}> 
+          <div className='flex items-center gap-5'>
+            <img src={session.user?.image || USER_IMAGE} alt="User" className='w-10 h-10 rounded-full' />
+            <button className='px-3 py-2 bg-gray-700 text-white rounded-full' onClick={() => signOut()}>
               <span className='hidden sm:block'>SIGN OUT</span>
+              <BiLogOut className='sm:hidden' />
             </button>
-            <Image src={USER_IMAGE} className='rounded-full' width={40} height={40} alt='User' />
-          </>
+          </div>
         ) : (
-          <button className='px-3 py-2 bg-gray-700 text-white rounded-full' onClick={() => signIn()}> 
-            <span className='hidden sm:block'>SIGN IN</span> 
-            <BiLogIn className='sm:hidden'/>
+          <button className='px-3 py-2 bg-gray-700 text-white rounded-full' onClick={() => signIn()}>
+            <span className='hidden sm:block'>SIGN IN</span>
+            <BiLogIn className='sm:hidden' />
           </button>
         )}
       </div>
